c98966045adb959f409d9c6aeb57e38d0fbab64d
Eleshin <awwaleleshin@gmail.com>
2023-07-17 06:47:37 -0700

    initial commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter != 'q' && letter != 'e')
+	{
+		putchat (letter);
+	}
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
