0547e8baae442ca8d371f5b0c0515d08289bdf64
Eleshin <awwaleleshin@gmail.com>
2023-07-23 23:48:48 -0700

    alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,16 +3,17 @@
 /**
  * print_alphabet_x10 - print alphabets
  */
+
 void print_alphabet_x10(void)
 {
 	int line, ch;
 	
 	for (line = 0; line <= 9; line++)
+
 	{
 		for (ch = 'a'; ch <= 'z'; ch++)
 			_putchar(ch);
 
 		_putchar('\n');
 	}
-
 }
